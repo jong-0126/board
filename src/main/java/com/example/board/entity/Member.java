d17ed36d7e6e084d8556f30de6979c1223d6e646
Master <baek011226@naver.com>
2025-04-02 11:00:53 +0900

    feat(entity): Spring 프로젝트 초기 엔티티 설계 추가

---
+++ src/main/java/com/example/board/entity/Member.java
@@ -0,0 +1,25 @@
+package com.example.board.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Integer age;
+
+
+
+}
