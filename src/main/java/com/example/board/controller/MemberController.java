1f045a13e0ebd1f22fc10124dbc23e4290bcc403
Master <baek011226@naver.com>
2025-04-02 11:31:57 +0900

    feat: Member 특정 회원 조회 기능 구현

--- src/main/java/com/example/board/controller/MemberController.java
+++ src/main/java/com/example/board/controller/MemberController.java
@@ -1,15 +1,13 @@
 package com.example.board.controller;
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpRequestDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
@@ -18,6 +16,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    //회원가입
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto){
 
@@ -25,5 +24,14 @@ public class MemberController {
 
         return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
 
+    }
+
+    //특정 회원 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponseDto> findById(@PathVariable Long id){
+
+        MemberResponseDto memberResponseDto = memberService.findById(id);
+
+        return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
     }
 }
